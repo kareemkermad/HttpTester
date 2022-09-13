fca5bfc90101f22b002b0e45db14bc6174705e85
Krimo <37254625+kareemkermad@users.noreply.github.com>
2022-09-13 21:08:45 +0100

    Update HttpTester.cpp

--- HttpTester.cpp
+++ HttpTester.cpp
@@ -1,22 +1,16 @@
 #include "httplib.h"
 #include <iostream>
+#include <chrono>
 
-int main()
+int main(int argc, char* argv[])
 {
     std::cout << "HTTP TESTER\n";
 
-    std::cout << "TEST GET:" << std::endl;
-
+    const long long time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    const std::string json = "{\"Timestamp\":" + std::to_string(time) + "}";
+    
     httplib::Client client("http://api.sparselab.com");
-    auto response = client.Get("/api/Report");
-    std::cout << "Error:" << response.error() << std::endl;
-    std::cout << "Status: " << response->status << std::endl;
-    std::cout << "Body: " << response->body << std::endl;
-
-    std::cout << "TEST POST:" << std::endl;
-
-    std::string json = "{\"RequestId\":\"900ea9c2-87ff-4072-8b8d-3952a367be2f\",\"Event\":\"Finished\",\"PeakMemoryConsumption\":19,\"TotalRunningTime\":0,\"SecretKey\":\"^ZQr26#i$eiSh@%ik9r20ztYLRb^3C@J\"}";
-    response = client.Post("/api/RequestTelemetry", json, "application/json");
+    auto response = client.Post("/api/StartupTimer", json, "application/json");
     std::cout << "Error:" << response.error() << std::endl;
     std::cout << "Status: " << response->status << std::endl;
     std::cout << "Body: " << response->body << std::endl;
